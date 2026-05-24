606be95b0d8096e7c9b81667ea0e7ac724e75cad
Sarang Kate <121939289+Skate-16@users.noreply.github.com>
2026-05-24 11:58:10 +0530

    Delete scripts directory

--- scripts/create-dev-cluster.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-kind get clusters | findstr /R /C:"^sk-idp$" >nul
-if errorlevel 1 (
-  kind create cluster --config kind\cluster.yaml
-) else (
-  echo kind cluster sk-idp already exists
-)
-kubectl cluster-info
